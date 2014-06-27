b2346312f589d5c7a5c535b6348642d02a58c1a6
Andreas Wenger <andi@xenoage.com>
2014-06-27 09:45:54 +0200

    Help added

--- src/com/xenoage/simplesync/Sync.java
+++ src/com/xenoage/simplesync/Sync.java
@@ -15,6 +15,7 @@ import com.xenoage.simplesync.server.ServerFile;
 import com.xenoage.simplesync.server.ServerIndex;
 import com.xenoage.utils.jse.io.DownloadTools;
 import com.xenoage.utils.jse.io.JseFileUtils;
+import com.xenoage.utils.jse.io.JseStreamUtils;
 import com.xenoage.utils.jse.security.Md5Sum;
 import com.xenoage.utils.jse.xml.XMLReader;
 
@@ -22,50 +23,7 @@ import com.xenoage.utils.jse.xml.XMLReader;
  * A very simple sync tool, that synchronizes files from a HTTP server
  * to a local directory.
  * 
- * The file paths are given in the local configuration file </code>sync.xml</code>.
- * 
- * Here is an example of the local </code>sync.xml</code> file:
- * <pre>{@literal
- * <sync server="http://files.xenoage.com" targetdir="lib/">
- *   <!-- directories which to clean -->
- *   <clean>
- *     <dir name="lib/"/>
- *   </clean>
- *   <!-- a single file -->
- *   <file file="gwt-servlet.jar" sourcedir="gwt/2.5.1/" targetdir="lib/webapp/war/WEB-INF/lib/"/>
- *   <!-- multiple files from the same source directory -->
- *   <fileset sourcedir="android-midi-lib/3.49/">
- *     <file file="android-midi-lib-3.49.jar"/>
- *     <file file="android-midi-lib-license.txt"/>
- *   </fileset>
- * </sync>
- * }</pre>
- * 
- * On each level, the local <code>targetdir</code> and the <code>sourcedir</code>
- * on the server can be defined. When missing, the directory from the parent
- * level is used.
- * 
- * Files, which are in the directories under <clean>, but are not listed in the files,
- * will be removed. The <clean> element is optional. When it misses, no old files will be deleted,
- * but just updated if required.
- * 
- * On the server side, the file <code>sync.index</code> is required at the root level
- * (which is the level of the <code>server</code> attribute in the </code>sync.xml</code>).
- * It lists the available directories, files and their md5 sums.
- * 
- * Here is an example of the remote <code>sync.index</code> file:
- * 
- * <pre>{@literal
- * <index>
- *   <dir name="android-midi-lib">
- *     <dir name="3.49">
- *       <file name="android-midi-lib-3.49.jar" md5="1e2dce28260eb0fe12ce83b07341b6a3"/>
- *       <file name="android-midi-lib-license.txt" md5="cafc597722cfad668c26d84e214993bb"/>
- *     </dir>
- *   </dir>
- * </index>
- * }</pre>
- * 
+ * For details how to use it, see the file <code>man.txt</code>.
  * 
  * @author Andreas Wenger
  */
@@ -75,6 +33,13 @@ public class Sync {
 	 * Starting point of the application.
 	 */
 	public static void main(String... args) {
+		//print help?
+		if (args.length > 0 && args[0].equals("--help")) {
+			String man = JseStreamUtils.readToString(Sync.class.getResourceAsStream("man.txt"));
+			System.out.println(man);
+			return;
+		}
+		
 		//create temp folder
 		File tempDir = JseFileUtils.createNewTempFolder();
 		
@@ -176,6 +141,7 @@ public class Sync {
 			
 		} catch (Exception ex) {
 			System.out.println("ERROR: " + ex);
+			System.out.println("Call SimpleSync with the --help parameter for help");
 			//delete temp folder
 			JseFileUtils.deleteDirectory(tempDir);
 			System.exit(1);
